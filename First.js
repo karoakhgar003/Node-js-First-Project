58932a002e7987dbf8f63536751e6d6886983ec3
karo <karoakhgar82@hotmail.com>
2022-12-20 20:54:32 +0330

    Server is Ready to pass data to second webserver

--- First.js
+++ First.js
@@ -1,7 +1,9 @@
 var http = require('http')
 
+const data = {name: 'karo', age: 18, email: 'karoakhgar82@hotmail.com'}
+
 http.createServer(function(req, res){
-        res.writeHead(200, {'Content-Type' : 'text/plain'})
-        res.end('hello world')
+        res.writeHead(200, {'Content-Type' : 'application\json'})
+        res.end(JSON.stringify(data))
 
 }).listen(1337,'127.0.0.1')

05566cc7ef95cd9f98af99aee2195335e9e147cb
karo <karoakhgar82@hotmail.com>
2022-12-22 14:40:31 +0330

    Data sent to Second server

--- First.js
+++ First.js
@@ -1,9 +1,22 @@
 var http = require('http')
+var axios = require('axios')
 
 const data = {name: 'karo', age: 18, email: 'karoakhgar82@hotmail.com'}
 
 http.createServer(function(req, res){
         res.writeHead(200, {'Content-Type' : 'application\json'})
         res.end(JSON.stringify(data))
+        
 
 }).listen(1337,'127.0.0.1')
+
+const api = axios.create({baseURL: 'http://127.0.0.1:1338'})
+api.post('/', {
+    data: JSON.stringify(data)
+})
+.then(res => {
+     console.log(res)
+})
+.catch(error => {
+     console.log(error)
+})

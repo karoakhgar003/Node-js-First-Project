dad4ba92960a3218338883461d626d8d94c509b6
karo <karoakhgar82@hotmail.com>
2022-12-27 15:01:37 +0330

    File saved

--- Third.js
+++ Third.js
@@ -1,9 +1,13 @@
 var http = require('http')
 var anyBody = require("body/any")
+fs = require('fs');
 
 http.createServer(function(req, res){
         anyBody(req, res, {}, (err,data)=>{
-                console.log(data)
+                fs.appendFile('data.txt', JSON.stringify(data), function (err) {
+                if (err) return console.log(err);
+                        console.log('File saved');
+                });
         })
         res.writeHead(200, {'Content-Type' : 'text/plain'})
         res.end('data recived')

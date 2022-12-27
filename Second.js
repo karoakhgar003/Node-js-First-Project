fad0bf2c4840cf7c7d72f5893ec3326516e9c0dc
karo <karoakhgar82@hotmail.com>
2022-12-27 14:30:50 +0330

    Handled data in the third server

--- Second.js
+++ Second.js
@@ -1,12 +1,27 @@
 var http = require('http');
-
+var axios = require('axios')
 var anyBody = require("body/any")
+
+
+let sendData = function(url,path,data) {
+        const api = axios.create({baseURL: url})
+        api.post(path, {
+        data: data
+        })
+        .then(res => {
+        console.log(res.data)
+        })
+        .catch(error => {
+        console.log(error)
+        })
+}
 
 http.createServer(function(req, res){
     let data;
     anyBody(req, res, {}, (err,data)=>{
         data = data.data
         let result = " My name is " + data.name + ".\n I'm " + data.age + " years old.\n My email is: " + data.email
+        sendData('http://127.0.0.1:1339','/',data)
         res.end(result)
     })
     res.writeHead(200, {'Content-Type' : 'text/plain'});
--- Third.js
+++ Third.js
@@ -1,7 +1,11 @@
 var http = require('http')
+var anyBody = require("body/any")
 
 http.createServer(function(req, res){
+        anyBody(req, res, {}, (err,data)=>{
+                console.log(data)
+        })
         res.writeHead(200, {'Content-Type' : 'text/plain'})
-        res.end('hello world3')
+        res.end('data recived')
 
 }).listen(1339,'127.0.0.1')
